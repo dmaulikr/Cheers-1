788415113b8f70b7a3184c0fab203dae91b119b5
Catherine Dong <cdong@cs.stanford.edu>
2017-03-13 00:00:43 -0700

    added drink buttons and color changing to manual tracker

--- Cheers/FluidViewController.swift
+++ Cheers/FluidViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 import BAFluidView
 import CoreMotion
 
-class FluidViewController: UIViewController {
-    
+class FluidViewController: UIViewController, DCPathButtonDelegate {
     // MARK: - Outlets
     
     @IBOutlet var swipeUpGesture: UISwipeGestureRecognizer!
@@ -25,24 +24,21 @@ class FluidViewController: UIViewController {
     let motionManager = CMMotionManager()
     
     var level = 0.0 //fluid level
-    var alcColors: [UIColor] = []
     var alcIndex = 0
-    
+    var cupButton:DCPathButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         startFluidAnimation()
-        loadDrinkOptions()
+        configureButtons()
     }
     
     @IBAction func swipeToAddDrink(_ sender: UISwipeGestureRecognizer) {
         level += 0.1 //incrementing by 10% right now
         fluidView?.fill(to: level as NSNumber!)
-        alcIndex += 1
-        alcIndex = alcIndex % alcColors.count
-        fluidView?.fillColor = alcColors[alcIndex]
+        fluidView?.fillColor = cupButton.selectedButton.itemColor
     }
     
     @IBAction func swipeToMinusDrink(_ sender: UISwipeGestureRecognizer) {
@@ -50,15 +46,29 @@ class FluidViewController: UIViewController {
         fluidView?.fill(to: level as NSNumber!)
     }
     
-    func loadDrinkOptions() {
-        let wineColor = UIColor(hex: "9B2942")
-        let cocktailColor = UIColor(hex: "EB7992")
-        let beerColor = UIColor(hex: "BAE278")
-        let shotColor = UIColor(hex: "BE823A")
-        alcColors = [wineColor, cocktailColor, beerColor, shotColor]
-        
+    public func pathButton(_ dcPathButton: DCPathButton!, clickItemButtonAt itemButtonIndex: UInt) {
+        fluidView?.fillColor = cupButton.selectedButton.itemColor
     }
     
+    func configureButtons() {
+        cupButton = DCPathButton(center: UIImage(named: "cup"), highlightedImage: UIImage(named: "cup"))
+        
+        cupButton.delegate = self
+        cupButton.dcButtonCenter = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height - 50)
+        cupButton.allowSounds = false
+        cupButton.allowCenterButtonRotation = true
+        cupButton.bloomRadius = 105
+
+        let beerButton = DCPathItemButton(image: UIImage(named: "beer"), imageMain: UIImage(named: "beer-main"), color: UIColor(hex: "E7B90B"))
+        let wineButton = DCPathItemButton(image: UIImage(named: "wine"), imageMain: UIImage(named: "wine-main"), color: UIColor(hex: "CC0F0F"))
+        let cocktailButton = DCPathItemButton(image: UIImage(named: "cocktail"), imageMain: UIImage(named: "cocktail-main"), color: UIColor(hex: "9AD145"))
+        let shotButton = DCPathItemButton(image: UIImage(named: "shot"), imageMain: UIImage(named: "shot-main"), color: UIColor(hex: "FF7902"))
+        
+        cupButton.addPathItems([beerButton!, wineButton!, cocktailButton!, shotButton!])
+        
+        self.view.addSubview(cupButton)
+
+    }
     
     func startFluidAnimation() {
         // set up accelerometer
