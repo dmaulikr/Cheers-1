fb59c0c1f044ae7e8586293a72b12d8414a66f85
Minna Xiao <mxiao26@stanford.edu>
2017-03-16 00:31:37 -0700

    nav change

--- Cheers/AppDelegate.swift
+++ Cheers/AppDelegate.swift
@@ -18,9 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let navigationBarAppearace = UINavigationBar.appearance()
-        navigationBarAppearace.tintColor = UIColor.white
+        navigationBarAppearace.tintColor = UIColor.black
         //navigationBarAppearace.barTintColor = UIColor(hex: "b3ed89")
-        navigationBarAppearace.barTintColor = UIColor.black
+        navigationBarAppearace.barTintColor = UIColor.white
         
         // Customize dialog appearance
         let pv = PopupDialogDefaultView.appearance()
