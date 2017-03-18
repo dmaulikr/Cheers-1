51e58a8f8645c1ac80f3beb136a749f25e2fe884
Emily Tang <emjtang@stanford.edu>
2017-03-17 17:02:34 -0700

    bc im anal about capitalization and standardization

--- Cheers/FriendsViewController.swift
+++ Cheers/FriendsViewController.swift
@@ -62,8 +62,8 @@ class FriendsViewController: UIViewController, UICollectionViewDataSource, UICol
     }
     
     @IBAction func bounce(_ sender: UIButton) {
-        let title = "going home now?"
-        let message = "we'll notify your group! tap anywhere outside this box to cancel."
+        let title = "Going home now?"
+        let message = "We'll notify your group! Tap anywhere outside this box to cancel."
         let popup = PopupDialog(title: title, message: message, image: nil)
         
         let button = DefaultButton(title: "BOUNCE") {
